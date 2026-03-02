c7e5329713058df5cb407775103f5cd2dcc90250
annhit29 <ann20010929@gmail.com>
2026-03-02 21:16:48 +0100

    change setting

--- gdprfs/settings.py
+++ gdprfs/settings.py
@@ -2,7 +2,10 @@ import os
 # Enforcement
 
 INSTRLIB_EXE = os.environ.get('INSTRLIB_EXE', '/home/ann20010929/MA3/Building_a_GDPR-compliant_file_system/enfguard/bin/enfguard.exe')
-INSTRLIB_FORMULA = os.environ.get('INSTRLIB_FORMULA', '/home/ann20010929/MA3/Building_a_GDPR-compliant_file_system/instrlib/gdprfs/policies/session.mfotl')
-INSTRLIB_SIG = os.environ.get('INSTRLIB_SIG', '/home/ann20010929/MA3/Building_a_GDPR-compliant_file_system/instrlib/gdprfs/policies/gdpr_draft.sig')
-INSTRLIB_LOG = os.environ.get('INSTRLIB_LOG', '/home/ann20010929/MA3/Building_a_GDPR-compliant_file_system/instrlib/gdprfs/gdprfstrace.log')
+INSTRLIB_FORMULA = os.environ.get('INSTRLIB_FORMULA', '/home/ann20010929/MA3/Building_a_GDPR-compliant_file_system/instrlib/gdprfs/policies/gdprfs.mfotl')
+# INSTRLIB_FORMULA = os.environ.get('INSTRLIB_FORMULA', '/home/ann20010929/MA3/Building_a_GDPR-compliant_file_system/instrlib/gdprfs/policies/session.mfotl')
+INSTRLIB_SIG = os.environ.get('INSTRLIB_SIG', '/home/ann20010929/MA3/Building_a_GDPR-compliant_file_system/instrlib/gdprfs/policies/gdprfs.sig')
+# INSTRLIB_SIG = os.environ.get('INSTRLIB_SIG', '/home/ann20010929/MA3/Building_a_GDPR-compliant_file_system/instrlib/gdprfs/policies/gdpr_draft.sig')
+INSTRLIB_LOG = os.environ.get('INSTRLIB_LOG', '/home/ann20010929/MA3/Building_a_GDPR-compliant_file_system/instrlib/gdprfs/gdprfs_trace.log')
+# INSTRLIB_LOG = os.environ.get('INSTRLIB_LOG', '/home/ann20010929/MA3/Building_a_GDPR-compliant_file_system/instrlib/gdprfs/gdprfstrace.log')
 
